8aa4b78fb2314032e46e9b733f9b32de76182397
Aleksandr_Sam <kukharchuk.aleksandr@gmail.com>
2017-08-16 01:53:16 +0300

    no message

--- GPS_without_I2C/GPS_without_I2C.ino
+++ GPS_without_I2C/GPS_without_I2C.ino
@@ -91,10 +91,46 @@ void send_data_to_lcd(void) // update LCD witd data
      if (age == TinyGPS::GPS_INVALID_AGE)
      {
        lcd.setCursor(4, 0);
-       lcd.print("WARNING!");
-       lcd.setCursor(0, 1);
-       lcd.print("No fix detected");
+       lcd.print("Loading!");
         playSoundState = false;
+
+        int chainLCD[16] = {};
+
+  for (int i = 0; i<55; i++)
+  {
+    if (chainLCD[0] == 0)
+    {
+      // init firs chain
+      chainLCD[0]++;
+    };
+
+    for (int j=1; j<16; j++)
+    {
+      if (chainLCD[j-1] == 4)
+      {
+        chainLCD[j]++;
+      };
+    };
+    for (int j=0; j<16; j++)
+    {
+      if (chainLCD[j] > 0 && chainLCD[j] < 10)
+      {
+        lcd.setCursor(j, 1);
+        lcd.write(chainLCD[j]-1);
+        chainLCD[j]++;
+   
+        if (chainLCD[j] == 10)
+        {    
+            lcd.setCursor(j, 1);
+            lcd.print(" ");
+        };
+      };
+    };
+    delay(200);
+    
+    GPS_Update();
+  }
+
        // car go go go
      }
      else if (age > 3000)
@@ -500,39 +536,28 @@ byte carAnim_1[8] = {
   0b00000,
   0b00000,
   0b00000,
-  0b10000,
-  0b10000,
   0b11000,
-  0b00000,
+  0b11000,
+  0b11100,
+  0b10000,
   0b00000
 };
 
 byte carAnim_2[8] = {
   0b00000,
   0b00000,
-  0b00000,
-  0b11100,
-  0b11100,
+  0b10000,
   0b11110,
-  0b10000,
-  0b00000
-};
-
-byte carAnim_3[8] = {
-  0b00000,
-  0b10000,
-  0b01000,
-  0b11111,
-  0b11111,
+  0b11110,
   0b11111,
   0b10100,
   0b01000
 };
 
-byte carAnim_4[8] = {
-  0b11000,
+byte carAnim_3[8] = {
+  0b10000,
+  0b01000,
   0b00100,
-  0b00010,
   0b11111,
   0b11111,
   0b11111,
@@ -540,48 +565,59 @@ byte carAnim_4[8] = {
   0b00010
 };
 
+byte carAnim_4[8] = {
+  0b11100,
+  0b01010,
+  0b01001,
+  0b11111,
+  0b11111,
+  0b11111,
+  0b10001,
+  0b00000
+};
+
 byte carAnim_5[8] = {
-  0b11110,
+  0b11111,
+  0b10010,
+  0b10010,
+  0b11111,
+  0b11111,
+  0b11111,
+  0b10100,
+  0b01000
+};
+
+byte carAnim_6[8] = {
+  0b00111,
+  0b00100,
+  0b00100,
+  0b01111,
+  0b01111,
+  0b11111,
+  0b00101,
+  0b00010
+};
+
+byte carAnim_7[8] = {
   0b00001,
-  0b00000,
-  0b11111,
-  0b11111,
-  0b11111,
+  0b00001,
+  0b00001,
+  0b00011,
+  0b00011,
+  0b00111,
   0b00001,
   0b00000
 };
 
-byte carAnim_6[8] = {
-  0b11111,
-  0b10000,
-  0b10000,
-  0b11111,
-  0b11111,
-  0b11111,
-  0b01000,
-  0b10000
-};
-
-byte carAnim_7[8] = {
-  0b00111,
-  0b00100,
-  0b00100,
-  0b11111,
-  0b11111,
-  0b11111,
-  0b01010,
-  0b00100
-};
-
 byte carAnim_8[8] = {
+  0b00000,
+  0b00000,
+  0b00000,
+  0b00000,
+  0b00000,
   0b00001,
-  0b00001,
-  0b00001,
-  0b01111,
-  0b01111,
-  0b11111,
-  0b00010,
-  0b00001
+  0b00000,
+  0b00000
 };
 //============================================
 
@@ -599,58 +635,14 @@ void showSplash() // show splash at the init
   lcd.createChar(5, carAnim_6); 
   lcd.createChar(6, carAnim_7); 
   lcd.createChar(7, carAnim_8); 
-  
-//  lcd.setCursor(0, 0); 
-//  lcd.print("\1");
 
+ 
 
-  int chainLCD[16] = {};
-//  lcd.setCursor(0, 0); 
-//  delay(3000);
-//  lcd.print(chainLCD[0]);
-
-  for (int i = 0; i<100; i++)
-  {
-    if (chainLCD[0] == 0)
-    {
-      // init firs chain
-      chainLCD[0]++;
-    };
-
-    for (int j=1; j<16; j++)
-    {
-      if (chainLCD[j-1] == 4)
-      {
-        chainLCD[j]++;
-      };
-    };
-    for (int j=0; j<16; j++)
-    {
-      if (chainLCD[j] > 0 && chainLCD[j] < 10)
-      {
-        lcd.setCursor(j, 1);
-        lcd.write(chainLCD[j]-1);
-        chainLCD[j]++;
-   
-        if (chainLCD[j] == 10)
-        {    
-            lcd.setCursor(j, 1);
-            lcd.print(" ");
-        };
-      };
-    };
-    delay(100);
-  }
-
-
-  lcd.setCursor(2, 0);
+  lcd.setCursor(0, 0);
   lcd.print("Rozumnii enot");
   
   lcd.setCursor(0, 1);
   lcd.print("GPS  speedometer");
-
-
-  
   
   delay(3000);
   lcd.clear();
